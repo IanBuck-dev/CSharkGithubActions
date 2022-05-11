05a3f1eb5406202d978fbaf166b2d9798e2a7832
IanBuck-dev <ian@hellohq.io>
2022-05-11 11:45:24 +0200

    added weather type

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,4 +9,13 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+    public Weather Weather { get; set; }
+}
+
+public enum Weather
+{
+    Cloudy,
+    Rainy,
+    Sunny,
+    Snowy
 }
